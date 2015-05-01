917ae2a0fe325fdfbe8e11f5aae88a170e796064
Christina Koch <christinakconnect@gmail.com>
2015-05-01 11:02:56 -0500

    adding scripts

--- combine_sheets.py
+++ combine_sheets.py
@@ -6,11 +6,11 @@ import sys
 file_list = sys.argv[1:]
 
 ##create PI list from one file
-df = pd.read_csv("madison_"+file_list[0], header=7, usecols=["PI"])
+df = pd.read_csv("madison_"+file_list[0], header=6, usecols=["PI"], skip_blank_lines=True)
 
 ##read in file stats, merge into master dataframe
 for file in file_list:
-    tmp = pd.read_csv("madison_"+file, header=7)
+    tmp = pd.read_csv("madison_"+file, header=6, skip_blank_lines=True)
     df = pd.merge(df,tmp, on="PI")
 
 ##format PI column
@@ -22,4 +22,3 @@ save_file = "madison"
 for item in tmp_list[-5:]:
 	save_file = save_file+"_"+item
 df.to_csv(save_file, index=False)
-
--- plotting.R
+++ plotting.R
@@ -1,8 +1,27 @@
 library(ggplot2)
 library(dplyr)
+library(reshape2)
 
-df = read.csv("madison_PI_2014-04-30_to_2015-04-30_aggregate.csv")
+df <- read.csv("madison_PI_2015-04-01_to_2015-04-30_aggregate.csv")
 
-df %>%
+total_hours <- df %>%
   mutate(PI = reorder(PI, CPU.Hours..Total)) %>%
-  ggplot(aes(x=CPU.Hours..Total, y=PI))+geom_point()
+  ggplot(aes(y=CPU.Hours..Total, x=PI)) + 
+  geom_bar(stat="identity", color="blue", fill="blue") + 
+  coord_flip() +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black")) +
+  labs(title="Total CPU Hours", x="CPU Hours", y="PI")
+
+cpu_usage <- df %>% 
+  select(PI,
+              max_cores = Job.Size..Max..Core.Count.,
+              min_cores = Job.Size..Min..Core.Count.,
+              avg_cores = Job.Size..Per.Job..Core.Count.) %>%
+  mutate(PI = reorder(PI, max_cores)) %>%
+  melt(value.name = "num_cores") %>%
+  ggplot(aes(x=num_cores,y=PI,color=variable)) + 
+  geom_point() + 
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black")) +
+  labs(title="Core Usage", x="Number of Cores", y="PI", color="")
