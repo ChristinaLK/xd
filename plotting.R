8e062b88756c1cbe19ba51a511732f0ec85dd2af
Christina Koch <christinakconnect@gmail.com>
2015-05-01 12:55:33 -0500

    visualization works

--- plotting.R
+++ plotting.R
@@ -2,26 +2,53 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 
-df <- read.csv("madison_PI_2015-04-01_to_2015-04-30_aggregate.csv")
+#file name w/o csv extension
+aggregate_file <- "madison_PI_2015-04-01_to_2015-04-30_aggregate"
 
+#load prepared csv
+df <- read.csv(paste0(aggregate_file,".csv"))
+colnames(df) <- c("PI","CPU_Hours","Max_Cores","Min_Cores","Avg_Cores")
+
+#create graph of total hours used
 total_hours <- df %>%
-  mutate(PI = reorder(PI, CPU.Hours..Total)) %>%
-  ggplot(aes(y=CPU.Hours..Total, x=PI)) + 
+  mutate(PI = reorder(PI, CPU_Hours)) %>%
+  ggplot(aes(y=CPU_Hours, x=PI)) + 
   geom_bar(stat="identity", color="blue", fill="blue") + 
   coord_flip() +
   theme(panel.background = element_blank(), 
         axis.line = element_line(colour = "black")) +
-  labs(title="Total CPU Hours", x="CPU Hours", y="PI")
+  labs(title="Total CPU Hours", y="CPU Hours", x="PI")
 
+#save total hours graph
+ggsave(paste0(aggregate_file,"_hours",".png"), 
+       plot = total_hours, 
+       width=6, 
+       height=4)
+
+#graph min/max/avg cpu usage
 cpu_usage <- df %>% 
   select(PI,
-              max_cores = Job.Size..Max..Core.Count.,
-              min_cores = Job.Size..Min..Core.Count.,
-              avg_cores = Job.Size..Per.Job..Core.Count.) %>%
-  mutate(PI = reorder(PI, max_cores)) %>%
-  melt(value.name = "num_cores") %>%
-  ggplot(aes(x=num_cores,y=PI,color=variable)) + 
-  geom_point() + 
+        Max_Cores,
+        Avg_Cores,
+        Min_Cores) %>%
+  mutate(PI = reorder(PI, Max_Cores)) %>%
+  melt(variable.name="Val", value.name = "Num_Cores") %>%  
+  ggplot(aes(x=PI, y=Num_Cores, color=Val)) + 
+  geom_point(size=4) + 
+  geom_linerange(data=df, aes(
+    x=PI, 
+    y=Avg_Cores, 
+    ymin=Min_Cores, 
+    ymax=Max_Cores, 
+    color="Range"),
+    color="black") +
+  coord_flip() + 
   theme(panel.background = element_blank(), 
         axis.line = element_line(colour = "black")) +
-  labs(title="Core Usage", x="Number of Cores", y="PI", color="")
+  labs(title="Core Usage", x="PI", y="Number of Cores", color="")
+
+#save CPU use plot
+ggsave(paste0(aggregate_file,"_CPU_use",".png"), 
+       plot = cpu_usage, 
+       width=6, 
+       height=4)
